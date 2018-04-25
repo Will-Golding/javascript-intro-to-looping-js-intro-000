4ebcaa25e21a0b9fc0274cafda6e2928d8515bb2
will Golding <willgolding117@gmail.com>
2018-04-25 13:33:33 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,10 +1,10 @@
 function forLoop(array){
   for(let i = 0; i<25; i++){
     if(i===1){
-      return "I am a strange loop";
+      array.push ("I am a strange loop");
     }
     else {
-      return `"I am ${i} strange loop"`;
+      array.push (`"I am ${i} strange loop"`);
     }
   }
   return array
